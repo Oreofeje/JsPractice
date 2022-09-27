66aef273eb43034c576409e133bb74a37ef50c99
Oreofeje <graceyforme@gmail.com>
2022-09-27 23:52:18 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,307 @@
+// let repeatedNumber = 10;
+
+// let xter = 😃;
+
+
+
+// if (xter = 😃){
+//     for (let xter = 0; xter < repeatedNumber; xter++) {
+//         const result = array[xter];
+        
+//     }
+// }
+
+// console.log(result);
+
+
+// let FAP = 0; 
+
+// for (let i =0; i< 10; i++) {
+//     FAP++;
+// }
+
+
+ 
+
+
+// let FAP = 13;
+
+// while (FAP < 20) {
+
+//     console.log('another person joined'); 
+
+//     FAP +=1;
+
+// }
+
+// // for loop 
+
+// let gip = 0;
+
+// for(let i =0; i< 90; i++)
+// {
+//     gip ++;
+// }
+
+// console.log(gip);
+
+
+
+
+
+// let spatans = 300;
+
+// for(i=0; i< 100; i++) {
+//     console.log('one more spatan added!')
+// }
+
+
+
+// let list = ['trio', 'duo', 'mono', 'nono'];
+
+// for(i=0; i< list.length; i++){
+//     // console.log(list)
+//     console.log(list[i])
+// }
+
+
+// let list2 = ['mama', 'papa', 'baba','bebe']
+
+// console.log(list2);
+// // console.log(list2[0])
+
+
+// for (let n of list2) {
+//     console.log(n)
+// }; 
+
+
+// // using loops with functions
+
+// let drum = ['bata', 'omele', 'gogon', 'masherati', 'shekere'];
+
+// drum.forEach(function ( n) {
+   
+//     console.log(n + ' ' + 'is my pleasant drum type.');
+// });
+
+
+
+// // eg of for
+// let person = [
+//     {name: 'Fisayo',
+//     age: 70,
+//     home: 'Goshem'
+//     },
+
+//     {name: 'sayo',
+//     age: 7,
+//     home: 'Goem'
+//     },
+
+//     {name: 'Hillary',
+//     age: 8,
+//     home: 'Niger'
+//     }
+// ];
+
+// for(j=0; j<person.length; j++){
+
+//     console.log(person[j].name);
+//     console.log(person[j].age);
+//     console.log(person[j].home);
+//     console.log('     ')
+// };
+
+
+
+
+
+// drum.forEach(function (n, index){
+
+//     console.log (index + '_' + n);
+
+// });
+
+
+
+// // while loop
+
+
+
+// // FUNCTION
+
+// function addNum (n1, n2) {
+//     return result = (n1 * n2 +10);
+    
+// }
+
+// console.log (addNum(8, 7));
+// console.log(result / 4);
+
+
+
+
+// // ########################################################################
+
+// function countTen() {
+    
+
+// // let count = 89;
+
+// for(count=1; count<=10; count++){
+
+
+//     document.getElementById('countHere').innerHTML += count + '<br>';
+// }
+// };
+
+
+
+// let ol = prompt('num1?');
+// let pl = prompt('num2?');
+
+// let rezult = (ol * pl);
+// alert (rezult);
+
+
+// for (rezult = (ol * pl); rezult<1000; ){
+//     prompt(ol)
+//     prompt(pl)
+// };
+
+// let p0e = 0;
+// while (p0e <20) {
+//     console.log(p0e++)
+// };
+
+// let p = 0;
+// while (p<4){
+//     console.log (p++);
+// }
+
+
+// do --while loop
+
+// let r= 0;
+
+// do{console.log (`value of r is ${r}`);
+// r++;
+// }
+
+// while (r<5);
+
+
+
+
+
+
+
+
+
+
+// for (l=0; l< 4; l++){
+//     // alert('yes')
+// }
+
+
+// for(m=0; m< drum.length; m++ ) {
+//     // alert(drum[m]);
+//     let ilu = `<div> I have this ${drum[m]} to show for my hardwork </div>`;
+//     // alert (ilu);
+// }
+
+
+// // while looping an array
+
+// let q= 0;
+// while (q < drum.length) {
+//     alert (`while looping ${drum[q]}`);
+//     q++
+// }
+
+
+
+
+// // if statements
+// let Debby = true;
+
+//  if(Debby){
+//     console.log(`boy is incorrect`);
+//  } else( !Debby)
+//     {
+//     console.log(`check your girl`);
+//  }
+ 
+
+// let user= false;
+
+
+// if (!user) {
+//     console.log('you are not logged in.')
+// }
+// else{
+//     console.log('you are logged in.')
+// }
+
+
+
+// breaks and continue
+
+const arrNum =[12,43,14,67,53,64,85,76.6,32,4.6,3,56];
+
+// for(n of arrNum)
+// {
+//     console.log(n + ' ' + 'here')
+// }
+ 
+// for( i=0; i<7; i++) 
+// {
+//     console.log (i);
+// }
+
+   
+
+// function
+function Ffname(){
+    let firstname = document.querySelector('#firstname').value;
+
+
+    let nameLength = firstname.length;
+
+    console.log(nameLength)
+    if 
+    (nameLength < 5 || nameLength > 50){
+        alert('input is invalid');
+    }
+    else{
+        alert ('good job!');
+    }
+    
+}
+
+let num = 2e-5;
+
+
+console.log(num);
+
+// looping through an array list of numbers
+
+let arrList =[9,45,3,2,66,24,87,4];
+
+let listEach  = 0;
+for (n of arrList){
+    document.write (
+        listEach = listEach+n);
+}
+
+
+
+
+
+
+
+
+
+
+
